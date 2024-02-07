30f4d468a517a74225ed8aa311b65b72a65b94b0
hailey.harney26 <hailey.harney26@d155.org>
2024-02-07 11:29:08 -0600

    delete set methods

--- src/Ball.java
+++ src/Ball.java
@@ -1,34 +1,69 @@
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Ball {
-   private int size;
+   private int width;
+
+   private int height;
     private Color color;
     private int speed;
     private int x,y;
+    int xLoc = 300; // where the ball is located
+    int xVel = 6;
+
+  //  int ran = (int)Math.random()*20;
 
 
 
-    public Ball(int x, int y){
-        this.x = x;
-        this.y = y;
-        size = 10;
-        speed = 6;
-        color = Color.RED;
 
+public Ball(){
+
+    this.x = x;
+    this.y = y;
+    x = (int)(Math.random()*400); // creating random location for the balls
+    y = (int)(Math.random()*400);
+    this.width = (int)(Math.random()*30); // initilize
+    this.height = 10; // initilize
+    this.speed = 6; // initilize
+    this.color = Color.RED; // initilize
+}
+
+
+    public Ball(int x, int y) {
+
+        if (xLoc >= getWidth() - 20) { // making the ball bounce
+            xVel = xVel * -1;
+        }
+    }
+
+
+    public int getWidth(){
+
+    return width;
+    }
+
+    public int setHeight(int height) {
+    int tall = setHeight(10);
+    return tall;
+    }
+    public int setWidth(int width){
+   int wide =  width;
+   return wide;
     }
 
 public void draw(Graphics g2){
     g2.setColor(color);
-    g2.fillOval(x,y, size, size);
+    g2.fillOval(x,y, height, width);
 }
 
-public void move(){
-    x+=speed;
+public void move() {
+
+    setWidth(width);
+    setHeight(height);
+
+    if(width>=500);
 }
-
-
-
 
 
 }
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -3,16 +3,18 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class MyPanel extends JPanel {
-    int xLoc = 300;
+    int xLoc = 300; // where the ball is located
     int xVel = 6;
     Ball myBall;
 
-   // ArrayList<Ball> cappy = new ArrayList<Ball>()
+    ArrayList<Ball> cappy = new ArrayList<Ball>();
 
     public MyPanel(){
         setBackground(Color.pink);
-        myBall = new Ball(200,300);
-      //  for(int i = 0; i<10; i++);
+        myBall = new Ball(200,300); // where the ball spawns
+        for(int i = 0; i<20; i++){
+            cappy.add(new Ball());
+        }
     }
 @Override
     public void paintComponent(Graphics g){
@@ -20,24 +22,24 @@ public class MyPanel extends JPanel {
    //System.out.println("hi");
 //    g.setColor(Color.cyan);
 //    g.fillOval(xLoc,80,10,10);
-    myBall.draw(g);
+    myBall.draw(g); // creating a ball
     myBall.move();
 //   xLoc+=xVel;
 
 
-   if(xLoc>=getWidth()-20){
-       xVel = xVel*-1;
-   }
+   //if(xLoc>=getWidth()-20){
+    //   xVel = xVel*-1;
+ //  }
    // g.fillOval(150,120,10,10);
    g.setColor(Color.yellow);
-    g.drawString("Hello world", 60,60);
-    g.drawRect(60,80,30,15);
-    g.drawRect(73,95,8,15);
-    g.drawRect(73,110,50,25);
-    g.drawRect(73,138,4,30);
+//    g.drawString("Hello world", 60,60);
+//    g.drawRect(60,80,30,15);
+//    g.drawRect(73,95,8,15);
+//    g.drawRect(73,110,50,25);
+//    g.drawRect(73,138,4,30);
 
     try {
-        Thread.sleep(100);
+        Thread.sleep(20); // speed of the ball
     }
     catch(Exception e) {
         System.out.println(e);
